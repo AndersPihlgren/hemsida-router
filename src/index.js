366e5bf653a96ca1c4d05a9d94fb9c5ea14ea8d7
AndersPihlgren <98736491+AndersPihlgren@users.noreply.github.com>
2022-11-08 15:30:13 +0100

    Hemsida med React Router

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./sidor/Layout";
+import Start from "./sidor/Start";
+import SkapaKonto from "./sidor/SkapaKonto";
+import Kontakt from "./sidor/Kontakt";
+import NoPage from "./sidor/NoPage";
+import Tjanster from "./sidor/Tjanster";
+import Inloggad from "./sidor/Inloggad";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Start />} />
+          <Route path="SkapaKonto" element={<SkapaKonto />} />
+          <Route path="Kontakt" element={<Kontakt />} />
+          <Route path="Tjanster" element={<Tjanster />} />
+          <Route path="Inloggad" element={<Inloggad />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App />);
---
+++ src/sidor/Start.js
@@ -0,0 +1,5 @@
+const Start = () => {
+    return <h1>Välkommen till Städafint AB!</h1>;
+  };
+  
+  export default Start;
